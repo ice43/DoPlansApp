b303511913609ead46d8fd966af9395c52838d16
bowski43 <43mngt@gmail.com>
2024-02-15 13:28:38 +0300

    Initial Commit

---
+++ DoPlansApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoPlansApp
+//
+//  Created by Serge Bowski on 2/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
